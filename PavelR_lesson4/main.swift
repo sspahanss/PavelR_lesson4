a14882bb471d2754f21b66951b227407903cf990
Parcifal <scar1987@gmail.com>
2020-07-26 14:38:58 +0800

    Initial Commit

---
+++ PavelR_lesson4/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PavelR_lesson4
+//
+//  Created by Павел on 26.07.2020.
+//  Copyright © 2020 Павел. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
